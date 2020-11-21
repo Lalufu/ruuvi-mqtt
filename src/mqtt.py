f1ff4c1cafce36b17ccf9240a061e5ba0e226ef9
Ralf Ertzinger <ralf@skytale.net>
2020-11-21 20:30:01 +0000

    Split out mqtt_main and ruuvi_main into separate modules

---
+++ src/mqtt.py
@@ -0,0 +1,102 @@
+"""
+Ruuvi-to-mqtt gateway
+
+This file contains the mqtt specific code
+"""
+
+import logging
+import threading
+import time
+import json
+
+import paho.mqtt.client as mqtt
+
+LOGGER = logging.getLogger(__name__)
+
+
+def mqtt_main(queue, config):
+    """
+    Main function for the MQTT process
+
+    Connect to the server, read from the queue, and publish
+    messages
+    """
+
+    def mqtt_on_connect(client, userdata, flags, rc):
+        """
+        Callback for the on_connect event
+
+        This is called from a different thread
+        """
+        nonlocal connected, connected_cv
+        LOGGER.debug("mqtt on_connect called, flags=%s, rc=%d", flags, rc)
+
+        with connected_cv:
+            connected = rc == 0
+            if connected:
+                connected_cv.notify()
+
+    def mqtt_on_disconnect(client, userdata, rc):
+        """
+        Callback for the on_disconnect event
+
+        This is called from a different thread
+        """
+        nonlocal connected, connected_cv
+        LOGGER.debug("mqtt on_disconnect called, rc=%d", rc)
+        if rc != 0:
+            # Unexpected disconnect
+            LOGGER.error("Unexpected disconnect from MQTT")
+
+        with connected_cv:
+            connected = False
+
+            # We do not have to wake up the waiter for this,
+            # because they'll just go back to sleep anyway
+
+    LOGGER.info("mqtt process starting")
+
+    # connected is tracking the connection state to MQTT.
+    # connected_cv is a condition variable that is protecing
+    # access to connected, because it is modified from a different
+    # thread.
+    connected = False
+    connected_cv = threading.Condition()
+
+    client = mqtt.Client(config["mqtt_client_id"])
+    client.on_connect = mqtt_on_connect
+    client.on_disconnect = mqtt_on_disconnect
+
+    # This will spawn a thread that handles events and reconnects
+    client.loop_start()
+
+    # We're going to loop until the connection succeeds, once
+    # it does the paho state machine will take care of reconnects
+    while True:
+        try:
+            client.connect(config["mqtt_host"], port=config["mqtt_port"])
+        except Exception as exc:
+            LOGGER.info(
+                "Could not connect to %s:%s, retrying (%s)",
+                config["mqtt_host"],
+                config["mqtt_port"],
+                exc,
+            )
+            time.sleep(2)
+            continue
+
+        break
+
+    while True:
+        # This will sleep unless we're connected
+        with connected_cv:
+            connected_cv.wait_for(lambda: connected)
+
+        data = queue.get(block=True)
+        LOGGER.debug("Read from queue: %s", data)
+
+        client.publish(
+            config["mqtt_topic"]
+            % {"mac": data["mac"], "name": data["ruuvi_mqtt_name"]},
+            json.dumps(data),
+        )
---
+++ src/ruuvi.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+"""
+Ruuvi-to-mqtt gateway
+
+This contains the Ruuvi specific parts
+"""
+
+import logging
+import math
+import time
+from collections import defaultdict
+
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+LOGGER = logging.getLogger(__name__)
+
+
+def ruuvi_main(queue, config):
+    """
+    Main function for the Ruuvi process
+
+    Read messages from BLE, and push them to the queue
+    """
+
+    # Used to track the last measurement we've seen, to avoid
+    # sending duplicate ones.
+    #
+    # Measurement numbers go up, normally, possibly skipping entries.
+    # They may also go down (when a Ruuvi reboots)
+    last_measurement = defaultdict(lambda: 0)
+
+    def dewpoint(temperature, humidity):
+        """
+        Calculate an approximate dewpoint temperature Tdp, given a temperature T
+        and relative humidity H.
+
+        This uses the Magnus formula:
+
+        N = ln(H / 100) + (( b * T ) / ( c + T ))
+
+        Tdp = ( c * N ) / ( b - N )
+
+        The constants b and c come from
+
+        https://doi.org/10.1175/1520-0450(1981)020%3C1527:NEFCVP%3E2.0.CO;2
+
+        and are
+        b = 17.368
+        c = 238.88
+
+        for temperatures >= 0 degrees C and
+
+        b = 17.966
+        c = 247.15
+
+        for temperatures < 0 degrees C
+        """
+
+        if temperature >= 0:
+            b = 17.368
+            c = 238.88
+        else:
+            b = 17.966
+            c = 247.15
+
+        N = math.log(humidity / 100) + ((b * temperature) / (c + temperature))
+
+        return (c * N) / (b - N)
+
+    def ruuvi_handle_data(found_data):
+        """
+        Callback function for tag data
+
+        Enrich the data with the current time, and push
+        to queue.
+
+        If the queue is full, drop the data.
+        """
+        nonlocal queue
+        nonlocal last_measurement
+
+        mac, data = found_data
+        lmac = mac.lower()
+
+        LOGGER.debug("Read ruuvi data from mac %s: %s", mac, data)
+
+        if "measurement_sequence_number" not in data or "mac" not in data:
+            LOGGER.error(
+                "Received measurement without sequence number or mac: %s", data
+            )
+            return
+
+        cur_seq = data["measurement_sequence_number"]
+        last_seq = last_measurement[data["mac"]]
+
+        if cur_seq == last_seq:
+            # Duplicate entry
+            LOGGER.debug(
+                "Received duplicate measurement %s from %s, ignoring",
+                cur_seq,
+                data["mac"],
+            )
+            return
+
+        last_measurement[data["mac"]] = cur_seq
+
+        # Process the data through offset functions
+        if lmac in config["offset_poly"]:
+            processed_data = {}
+            for key, value in data.items():
+                if key in config["offset_poly"][lmac]:
+                    # Ruuvi sends data with two significant digits,
+                    # round the scaled data as well
+                    processed_data[key] = round(
+                        config["offset_poly"][lmac][key](value), 2
+                    )
+                    processed_data["ruuvi_mqtt_raw_%s" % (key,)] = value
+                else:
+                    processed_data[key] = value
+
+            data = processed_data
+
+        # Add the dew point temperature, if requested
+        if config["dewpoint"]:
+            data["ruuvi_mqtt_dewpoint"] = round(
+                dewpoint(data["temperature"], data["humidity"]), 2
+            )
+
+        LOGGER.debug("Processed ruuvi data from mac %s: %s", mac, data)
+
+        # Find the device name, if any
+        # Use the `mac` field as a fallback
+        data["ruuvi_mqtt_name"] = config["macnames"].get(lmac, data["mac"])
+
+        # Add a time stamp. This is an integer, in milliseconds
+        # since epoch
+        data["ruuvi_mqtt_timestamp"] = int(time.time() * 1000)
+
+        try:
+            queue.put(data, block=False)
+        except queue.Full:
+            # Ignore this
+            pass
+
+    LOGGER.info("ruuvi process starting")
+
+    RuuviTagSensor.get_datas(ruuvi_handle_data, [x.upper() for x in config["filter"]])
